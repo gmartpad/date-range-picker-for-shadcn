f42f57b7c17b55fa30d7f2f7c5a51748b05f3030
gmartpad <contato.gabrielmartinspadoin@gmail.com>
2025-08-11 17:04:05 -0300

    [DATE] add-pt-br-localization - chore: removing rollup

--- dist/src/date-input.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface DateInputProps {
-    value?: Date;
-    onChange: (date: Date) => void;
-}
-declare const DateInput: React.FC<DateInputProps>;
-export { DateInput };
--- dist/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { DateRangePicker } from './date-range-picker';
-export { DateInput } from './date-input';
-export type { DateRangePickerProps } from './date-range-picker';
