ae81fe1f53050359100a2ff378ace3f06b30e82a
gmartpad <contato.gabrielmartinspadoin@gmail.com>
2025-08-11 16:50:42 -0300

    [DATE] add-pt-br-localization - fix/chore: react now is a peerDependency

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ import typescript from 'rollup-plugin-typescript2';
 
 export default {
   input: 'src/index.ts',
+  external: ['react', 'react-dom'],
   output: [
     {
       file: 'dist/index.js',
